2110ac84ba5ffa03a86db51172731b19c9435960
Mar-gt0 <marlent605@gmail.com>
2025-10-19 20:16:27 -0600

    Hice la primers Screen de la pantalla de recuperar contraseña

--- app/src/main/java/mx/edu/utez/tiendatenis/ui/screens/RecuContraScreen.kt
+++ app/src/main/java/mx/edu/utez/tiendatenis/ui/screens/RecuContraScreen.kt
@@ -1,6 +1,79 @@
 package mx.edu.utez.tiendatenis.ui.screens
 
+import android.util.Log.i
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Label
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import mx.edu.utez.tiendatenis.ui.theme.TiendaTenisTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import mx.edu.utez.tiendatenis.R
+
+
+
+
 
 @Composable
-fun RecuContra
+fun RecuContraScreen( ){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.tennislogo),
+            contentDescription = "Logo de nuestra app"
+        )
+
+        Text(
+            text =  "Ingresa tu correo electronico"
+        )
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Correo electrónico") }
+        )
+
+
+
+
+        Button(
+            onClick = {}
+        ) {
+            Text(
+                text = "Continuar"
+            )
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRecuContraScreen(){
+    TiendaTenisTheme {
+        RecuContraScreen()
+    }
+}
